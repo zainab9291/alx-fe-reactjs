e2b6558b2a804eb73088f1ee5e557c4b8414346d
Zainab Ashraf <zainab9291@gmail.com>
2025-07-21 00:45:44 +0300

    add

---
+++ alx-react-app-props 1/src/App 1.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage'; // تأكدي من مسار الاستيراد
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-company/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
