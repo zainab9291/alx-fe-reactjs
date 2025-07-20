c20696d24c557112b66ee371645ddb8112381fb3
Zainab Ashraf <zainab9291@gmail.com>
2025-07-20 21:56:45 +0300

    Add WelcomeMessage JSX component and integrate into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css';
-import WelcomeMessage from './components/WelcomeMessage'; // تأكدي من مسار الاستيراد
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <WelcomeMessage />
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
