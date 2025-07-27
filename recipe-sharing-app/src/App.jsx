4fd214efeb89759af0b9b91336e73e83b99a4c70
Zainab Ashraf <zainab9291@gmail.com>
2025-07-27 14:15:31 +0300

    Add

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,27 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
-      <h1>Recipe Sharing App</h1>
-      <Routes>
-        {/* Home page route */}
-        <Route
-          path="/"
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          }
-        />
-        {/* Details page for each recipe */}
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
+        <h1>Recipe Sharing App</h1>
+        <Routes>
+          {/* Home page route */}
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          {/* Details page for each recipe */}
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
