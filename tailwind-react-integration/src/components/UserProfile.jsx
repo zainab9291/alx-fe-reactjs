09a82666145c8420d797bcac99c623605f1ce11c
Zainab Ashraf <zainab9291@gmail.com>
2025-08-11 00:08:08 +0300

    Styled UserProfile component responsive with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+        className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto transform transition-transform duration-300 ease-in-out hover:scale-110"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center transition-colors duration-300 ease-in-out hover:text-blue-500">
         John Doe
       </h1>
       <p className="text-sm md:text-base text-gray-600 text-center">
