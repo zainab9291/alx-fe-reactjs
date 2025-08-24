b2f96338754b4cd5a0c65498d65213cfc4a9337f
Zainab Ashraf <zainab9291@gmail.com>
2025-08-25 01:06:46 +0300

    feat: todo list with add/toggle/delete + Jest & RTL tests (Vite v4)

--- react-todo/jest.config.cjs
+++ react-todo/jest.config.cjs
@@ -1,9 +1,14 @@
+// jest.config.cjs
 module.exports = {
-  testEnvironment: 'jsdom',
-  transform: { '^.+\\.[jt]sx?$': 'babel-jest' },
-  moduleFileExtensions: ['js', 'jsx'],
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  testEnvironment: "jsdom", // عشان React components تشتغل
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest", // تحويل ملفات React
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom"], // عشان نستعمل matchers زي toBeInTheDocument
+  moduleNameMapper: {
+    // لو عندك استايلز أو صور ممكن تتعمل mock
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/__mocks__/fileMock.js",
+  },
 };
