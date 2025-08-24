698d0505e6fe2d8b3046d2f38fcb51a804498ce8
Zainab Ashraf <zainab9291@gmail.com>
2025-08-24 23:48:19 +0300

    number 2

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import AddRecipeForm from "./components/AddRecipeForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <AddRecipeForm />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100 p-6">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
